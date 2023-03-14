f064767bfb6cd35fb2301d0232d686a11ac61e5c
eearly01 <elijah.early@gmail.com>
2023-03-14 12:48:09 -0500

    added city

--- models/dayModel.js
+++ models/dayModel.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const daySchema = new mongoose.Schema({
 	city: String,
-	temperature: { type: Number, required: true },
+	temp: { type: Number, required: true },
 	windSpeed: { type: Number, required: true },
 	windDirection: String,
 	probabilityOfPrecipitation: { value: {type: Number, default: 0} },
