0402af6a92b8c0251f1e6db1c2f45651c74981de
eearly01 <elijah.early@gmail.com>
2023-03-14 14:17:24 -0500

    changed windspeed to string

--- models/dayModel.js
+++ models/dayModel.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const daySchema = new mongoose.Schema({
 	city: String,
 	temp: { type: Number, required: true },
-	windSpeed: { type: Number, required: true },
+	windSpeed: { type: String, required: true },
 	windDirection: String,
 	probabilityOfPrecipitation: { value: {type: Number, default: 0} },
 	detailedForcast: String,
