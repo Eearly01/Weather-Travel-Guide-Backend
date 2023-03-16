182bd5f8214f345968c8a33e390f1cb53f282d85
eearly01 <elijah.early@gmail.com>
2023-03-16 12:03:03 -0500

    added tempUnit and day name

--- models/dayModel.js
+++ models/dayModel.js
@@ -2,8 +2,10 @@ const mongoose = require('mongoose');
 
 const daySchema = new mongoose.Schema({
 	number: {type: Number},
+	dayName: {type: String, default: 'WeekDay', required: true},
 	city: String,
 	temp: { type: Number, required: true },
+	tempUnit: {type: String},
 	windSpeed: { type: String, required: true },
 	windDirection: String,
 	probabilityOfPrecipitation: { value: {type: Number, default: 0} },
